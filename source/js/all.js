2941d94c702ba70615930bcbf63ee1e3edfa0709
Paul Nerger <pnerger@gmail.com>
2016-08-09 19:34:08 -0700

    Refactored how JavaScript is used to simplify the logic

--- source/js/all.js
+++
@@ -1,6 +0,0 @@
-//= require ./lib/_energize
-//= require ./app/_lang
-//= require ./app/_search
-//= require ./lib/_jquery_ui
-//= require ./lib/_jquery.tocify
-//= require ./lib/_imagesloaded.min
--- source/js/all_nosearch.js
+++
@@ -1,5 +0,0 @@
-//= require ./lib/_energize
-//= require ./app/_lang
-//= require ./lib/_jquery_ui
-//= require ./lib/_jquery.tocify
-//= require ./lib/_imagesloaded.min
